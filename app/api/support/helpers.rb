0ba6f6970d4446f91745da3fc9e62a57d8ba12b5
Andrew Rempe <arempe@k2p.com>
2017-11-28 09:18:52 -0500

    add more api helpers

--- app/api/support/helpers.rb
+++ app/api/support/helpers.rb
@@ -2,12 +2,32 @@ module API
   module Support
     module Helpers
       def set(params)
-        params.delete :auth_token
-        declared(params).to_hash.compact.deep_symbolize_keys
+        declared(params, include_parent_namespaces: false,
+                         include_missing: false)
       end
 
-      def validate!(model, code = '422')
-        unprocessable! code, "#{model.class.name}: #{model.errors.full_messages.join(', ')}" unless model.valid?
+      def find(collection, query, code = nil)
+        collection.find_by(query).tap do |m|
+          unless m
+            class_name = collection.respond_to?(:model) ? collection.model.name : collection
+            not_found!(*["#{class_name} with query '#{query}' was not found", code].compact)
+          end
+        end
+      end
+
+      def paginate(query, per_page: params[:per_page], page: params[:page])
+        total_rows = query.unscope(:select).count
+
+        present :total_rows, total_rows
+        present :total_pages, (total_rows / per_page.to_f).ceil
+
+        query.limit(per_page).offset((page - 1) * per_page)
+      end
+
+      def sort(query, sort_by: params[:sort_by], sort_direction: params[:sort_direction])
+        # not using hash syntax because Rails will prefix the table name of the collection
+        # model (users.last_name), and that could be undesired behavior (eg. joins)
+        query.order("#{sort_by} #{sort_direction}")
       end
     end
   end
