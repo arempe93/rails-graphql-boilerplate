06e0ee1854f9239e511f5e88fb77c6875da16136
Andrew Rempe <andrewrempe@gmail.com>
2019-12-27 20:23:07 -0600

    remove custom code leftover

--- app/graphql/mutations/base_mutation.rb
+++ app/graphql/mutations/base_mutation.rb
@@ -6,30 +6,12 @@ module Mutations
 
     protected
 
-    def push_event(action:, subjects:, data: nil)
-      EventService.push(action: action,
-                        device_id: context[:device_id],
-                        user_id: context[:user_id],
-                        request_id: context[:request][:id],
-                        jti: context[:token]&.jti,
-                        subjects: subjects,
-                        data: data)
-    end
-
     def with_void_return
       nil.tap { yield }
     end
 
     def error!(**kwargs)
       raise Errors::BaseError.new(**kwargs)
-    end
-
-    def forbidden!(msg = 'Not allowed to access this resource')
-      error! message: msg, code: :FORBIDDEN
-    end
-
-    def unauthorized!(msg = 'Authentication required')
-      error! message: msg, code: :UNAUTHORIZED
     end
   end
 end
--- app/graphql/scalars/base_scalar.rb
+++ app/graphql/scalars/base_scalar.rb
@@ -2,8 +2,5 @@
 
 module Scalars
   class BaseScalar < GraphQL::Schema::Scalar
-    def self.default_graphql_name
-      name.split('::').last.sub('Scalar', '')
-    end
   end
 end
